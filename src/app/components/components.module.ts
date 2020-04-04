f9469b4ba5e3a256b88a5cbb1f6bc0acfc1e091e
= <celotts@gmail.com>
2020-04-04 00:11:42 -0300

    Fixer
    Se aplica ajuste

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,16 +6,15 @@ import { ColorsComponent } from './colors/colors.component';
 import { ColorGridComponent } from './colors/color-grid/color-grid.component';
 import { PaginacionGridColorComponent } from './colors/paginacion-grid-color/paginacion-grid-color.component';
 import { ColorCopyComponent } from './colors/color-copy/color-copy.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [ColorsComponent, ColorGridComponent, PaginacionGridColorComponent, ColorCopyComponent, NotFoundComponent],
   imports: [
     CommonModule,
-    SharedModule,
+    SharedModule
   ],
-  exports: [ColorsComponent, ColorGridComponent, PaginacionGridColorComponent, ColorCopyComponent],
+  exports: [ColorsComponent, ColorGridComponent, PaginacionGridColorComponent, ColorCopyComponent, NotFoundComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class ComponentsModule { }
