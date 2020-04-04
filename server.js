7eeedb373fc4e81333e216cbf8f23a79bf5b63ee
= <celotts@gmail.com>
2020-04-04 11:50:55 -0300

    Mejoras para deploy heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
-app.use(express.static(__dirname + '/dist/ng-blog'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ng-blog/index.html'));
+app.use(express.static(__dirname + '/dist));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+console.log('console listening!');
