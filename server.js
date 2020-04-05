edebd6e1290c89bfb2b37774aae24ebabaea23d5
= <celotts@gmail.com>
2020-04-04 22:00:11 -0300

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/app-colory'))
+app.use(express.static(__dirname + '/dist/app-color'))
 app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/app-color/index.html'));
