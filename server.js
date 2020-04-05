d961e8da3752a9c351ed56d62b5ba2110dc85491
= <celotts@gmail.com>
2020-04-05 11:54:32 -0400

    .

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var express = require("express");
+const express = require('express');
 
+const path = require('path');
 
-var app = express();
+const app = express();
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+const port = process.env.PORT || 8080;
+
+app.set('port', port);
+
+app.use(express.static(__dirname + '/dist/'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log('console listening!' + app.get('port'));
+})
