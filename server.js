f440c0efed65bc52664297a425ff770f1db43f09
= <celotts@gmail.com>
2020-04-04 22:19:22 -0300

    .

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/appColor'))
+
 app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/appColor/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/app-color/index.html'));
 });
-
+app.use(express.static(__dirname + '/dist/app-color'))
 console.log('console listening!');
