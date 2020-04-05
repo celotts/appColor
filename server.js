6a465c2ccaed954676c4caf1e66c8dbbc1000703
= <celotts@gmail.com>
2020-04-04 22:57:40 -0300

    .

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 app.use(express.static(__dirname + '/dist/appColor'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/appColor/index.html'));
+  res.set('Content-Type', 'text/thml')
+    .sendFile(path.join(__dirname, '/dist/appColor/index.html'));
 });
 
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('console listening!');
-}
+})
 
