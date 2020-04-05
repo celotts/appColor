6bd3707e0489d4fc560a849c8fc8eeae843b9f22
= <celotts@gmail.com>
2020-04-04 23:05:11 -0400

    .

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 8080;
 
 app.set('port', port);
 
-app.use(express.static(__dirname + '/dist/'))
+app.use(express.static(__dirname + '/dist/https://dashboard.heroku.com/apps/373a870b-ae2a-40d6-a6d3-3ed31af5a9e2/activity/builds/7172c6b2-3646-4e14-b0f0-9aa27ad12313'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/https://dashboard.heroku.com/apps/373a870b-ae2a-40d6-a6d3-3ed31af5a9e2/activity/builds/7172c6b2-3646-4e14-b0f0-9aa27ad12313/index.html'));
 });
 
 app.listen(app.get('port'), () => {
