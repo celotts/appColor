b38a46b0286d2a2cff695b02cc01632d4f4711e0
= <celotts@gmail.com>
2020-04-04 22:28:16 -0300

    .

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const app = express();
+
 const path = require('path');
+const app = express();
 
+app.use(express.static(__dirname + '/dist/appColor'))
 
-app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/app-color/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/appColor/index.html'));
 });
-app.use(express.static(__dirname + '/dist/app-color'))
-console.log('console listening!');
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('console listening!');
+}
+
