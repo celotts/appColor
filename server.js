823eceee02f356bea5eab34b28239981b4e1bb36
= <celotts@gmail.com>
2020-04-04 23:43:55 -0300

    .

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const port = process.env.PORT || 8080;
 
 app.set('port', port);
 
-app.use(express.static(__dirname + '/dist/appColor'))
+app.use(express.static(__dirname + '/dist/'))
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/appColor/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
-
 
 app.listen(app.get('port'), () => {
   console.log('console listening!' + app.get('port'));
